858699ae40d3d631ccbfc02de933cad9a20af620
shivendra2008 <73811400+shivendra2008@users.noreply.github.com>
2021-04-23 10:10:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -24,25 +24,6 @@ function modelLoaded()
 }
 
   
-function check() {
-    img = document.getElementById('captured_img');
-    classifier.classify(img, gotResult);
-  }
-  
-  
-  function gotResult(error, results) 
-  {
-    if (error) 
-    {
-      console.error(error);
-    }
-    else 
-    {
-      console.log(results);
-      document.getElementById("result_person_name").innerHTML = results[0].label;
-      document.getElementById("result_person_accuracy").innerHTML = results[0].confidence.toFixed(3);
-    }
-}
 
 
 
